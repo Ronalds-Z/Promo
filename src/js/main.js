b1be8fd3c6294d002f5b2ef37928ea991ac29dbd
Ronalds-Z <ronaldsz123@inbox.lv>
2024-01-30 23:31:59 +0200

    added swiper for dishes/ fixed some layout issues/ added alt tag to all img elements

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,42 @@
 import "/src/scss/main.scss";
 
 import Swiper from 'swiper';
-import { Navigation} from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 
-const swiper = new Swiper('.swiper', {
+const swiperGallery = new Swiper('.gallery .swiper', {
     modules: [Navigation],
     loop: true,
-    slidesPerView: 3,
+    centeredSlides: true,
+    slidesPerView: 1.5,
+    breakpoints: {
+        992: {
+            slidesPerView: 3,
+        },
+    },
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.gallery .swiper-button-next',
+        prevEl: '.gallery .swiper-button-prev',
+    },
+});
+const swiperDishes = new Swiper('.dishes .swiper', {
+    modules: [Navigation],
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 1.5,
+    centeredSlides: true,
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+        },
+        1100: {
+            slidesPerView: 6,
+            centeredSlides: false,
+        }
+    },
+
+    navigation: {
+        nextEl: '.dishes .swiper-button-next',
+        prevEl: '.dishes .swiper-button-prev',
     },
 });
