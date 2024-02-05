2487bf0a00ab40cf5309ef21e7a28257d75e6447
Ronalds-Z <ronaldsz123@inbox.lv>
2024-02-05 21:12:29 +0200

    added mobile nav menu

--- src/js/main.js
+++ src/js/main.js
@@ -40,3 +40,40 @@ const swiperDishes = new Swiper('.dishes .swiper', {
         prevEl: '.dishes .swiper-button-prev',
     },
 });
+const burgerButton = document.querySelectorAll('.burger-button')
+const header = document.querySelector('.header')
+const navMenu = document.querySelector('.header-bottom')
+if (burgerButton) {
+    burgerButton.forEach((burgerButton) => {
+        burgerButton.addEventListener('click', () => {
+            navMenu.classList.toggle('open-nav-menu')
+            header.classList.toggle('blur-background');
+        });
+    });
+
+}
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 1200) {
+        navMenu.classList.remove('open-nav-menu');
+        header.classList.remove('blur-background');
+        menuTexts.classList.remove('open-list')
+    }
+})
+
+const menuButtons = document.querySelectorAll('.nav-button');
+const menuTexts = document.querySelectorAll('.nav-list-item');
+if (menuButtons.length && menuTexts.length) {
+    menuButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            const viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+            if (viewportWidth <= 1200) {
+                menuTexts[index].classList.toggle('open-list');
+                if (menuTexts[index].classList.contains('open-list')) {
+                    menuTexts[index].style.maxHeight = menuTexts[index].scrollHeight + 'px';
+                } else {
+                    menuTexts[index].style.maxHeight = 0;
+                }
+            }
+        });
+    });
+};
